0a2d03c245fb176831e4af634972c1daa1b5e8e3
Emin Mammadov <42080527+herobaku@users.noreply.github.com>
2024-09-23 21:10:12 +0400

    fix css and edited main.js

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const burger = document.querySelector(".burger");
+const change = document.querySelectorAll(".burger svg");
+const modal = document.querySelector(".modal");
 
+// Hidden close icon
+change.forEach((item, index) => {
+    if (index === 1) {
+        item.style.display = "none";
+    }
+});
+
+// Visible Burger Menu
 burger.addEventListener("click", (e) => {
-    burger.classList.toggle("active");
-})
+    change.forEach((item, index) => {
+        if (item.style.display === "none") {
+            item.style.display = "block";
+            document.body.classList.add("no-scroll");
+            if(!modal.classList.contains("active")){
+                modal.classList.add("active");
+            } else {
+                modal.classList.remove("active");
+            }
+        } else {
+            item.style.display = "none";
+            document.body.classList.remove("no-scroll");
+        }
+    });
+});
